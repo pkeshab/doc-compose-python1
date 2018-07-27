cc339b330b7a5a0b8fad00659d44d1526807e3ff
keshab <keshabpandey12@gmail.com>
2018-07-27 16:33:37 -0400

    This is the file for showing the docker compose exmaple with .py script

---
+++ simple_app/content/code/simple_app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from redis import Redis
+ 
+app = Flask(__name__)
+redis = Redis(host='redis', port=6379)
+ 
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return '<b>You have successfully completed the concept of docker-compose!</b><br><br>Did you learn any new cool things here?<br><br>You came and clicked this page for  {} times.\n'.format(count)
+ 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
